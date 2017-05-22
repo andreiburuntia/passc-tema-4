11e227e6fdb7a528cf62ba1c794c090903983578
Andrei Buruntia <andreiburuntia@gmail.com>
2017-05-22 21:30:56 +0300

    all

---
+++ Tema4PASSC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4PASSC
+{
+   public class Student
+   {
+      public string name { get; set; }
+      public string id { get; set; }
+      public double grade { get; set; }
+   }
+}
---
+++ Tema4PASSC/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4PASSC
+{
+   class StudentRepository
+   {
+      public List<Student> GetStudents()
+      {
+         StudentDBContext studentDBContext = new StudentDBContext();
+         return studentDBContext.Students.ToList();
+      }
+   }
+}
